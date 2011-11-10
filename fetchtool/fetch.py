796b83a57d675f3285c46ab69e822d3d5b3a6510
Also <alsotang@gmail.com>
2011-11-10 15:05:53 +0800

    写不出多线程抓取，搁浅！！

--- fetchtool/fetch.py
+++ fetchtool/fetch.py
@@ -14,16 +14,41 @@
 '''
 import urllib2
 import parsePage
+import logging
 
-def fetchPage(stuid):
+def fetchPage(stuid, datalist):
     '''返回unicode形式的页面源码'''
 
     url = "http://www.scupec.com:4489/jncx/jncx.asp?textxh="
-    result = urllib2.urlopen(url + stuid, timeout = 10)
-    return result.read().decode('gbk')
+    result = urllib2.urlopen(url + stuid, timeout = 5)
+    data = parsePage.parsePage(result.read().decode('gbk'))
+    if data != False:
+        datalist.append(data)
+    else:
+        return False
+    return True
+
+def fetchCollege(stuid):
+    datalist = [] #一个学院的数据放进一个列表里面
+    ori_stuid = stuid[0: 7]
+    datafile = open(ori_stuid + '.txt', 'w')
+
+    error = 0
+    try:
+        for i in xrange(999):
+            stuid = ori_stuid + "%03d" % i
 
 
+
+    except FetchCollegeError:
+        pass
+    return datalist
+
+
+
+class FetchCollegeError(Exception):
+    pass
 
 if __name__ == '__main__':
-    print fetchPage("1043111063")
+    print fetchCollege("1043111063")
 
